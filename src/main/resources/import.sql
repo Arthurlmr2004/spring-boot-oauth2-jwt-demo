76574d20d248ea3fb815f332c8534e7c38ddf081
Arthur Rocha <arthurlarangeiras@gmail.com>
2025-03-04 23:26:55 -0300

    Adicionando o BCrypt

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$1sLS5GrPB01j2ZJhPP4n..wyf8M9wm267EdAfxdUwvGxlOwAU.ICy');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$1sLS5GrPB01j2ZJhPP4n..wyf8M9wm267EdAfxdUwvGxlOwAU.ICy');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
